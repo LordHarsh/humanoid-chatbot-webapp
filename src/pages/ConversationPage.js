a6e33b08848607c16387b86f2fa5ce6b456aff04
Harsh Banka <harshbanka321@gmail.com>
2023-07-11 12:34:47 +0530

    fixes

--- src/pages/ConversationPage.js
+++ src/pages/ConversationPage.js
@@ -10,6 +10,7 @@ const ConversationPage = () => {
   delete configuration.baseOptions.headers["User-Agent"];
   const openai = new OpenAIApi(configuration);
 
+  let error_count = useRef(0);
   const [error, setError] = useState(null);
   const [sending, setSending] = useState(null);
   const { conversations, dispatch } = useConversationContext();
@@ -23,58 +24,59 @@ const ConversationPage = () => {
     scrollToBottom();
   }, [conversations]);
 
-  useEffect(() => {
-    const handleSubmit = async () => {
-      if (conversations.length > 0) {
-        const lastMessage = conversations[conversations.length - 1];
-        if (lastMessage.role === "user") {
-          setError(false);
-          setSending(true);
-
-          try {
-            const completion = await openai.createChatCompletion({
-              model: "gpt-3.5-turbo",
-              messages: conversations,
-            });
-            const model_message = completion.data.choices[0].message.content;
-
-            await dispatch({
-              type: "ADD_CHAT",
-              payload: { role: "assistant", content: model_message.trim() },
-            });
-
-            setSending(false);
-          } catch (error) {
-            console.log(error);
-            setError(true);
-            await dispatch({
-              type: "ADD_CHAT",
-              payload: {
-                role: "assistant",
-                content:
-                  "There was a server overload. This is because we are currently on a free trial version of the API. In the future, we are planning to have some custom models.",
-              },
-            });
-          }
-        }
-      }
-    };
-
-    handleSubmit();
-  }, [conversations, dispatch, openai]);
-
-  const handleSubmitForm = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const input = e.target.message;
-    const user_message = input.value;
+    setError(false);
+    setSending(true);
+    e.target.submit.disabled = true;
+    e.target.message.disabled = true;
+    const user_message = input.value.trim();
     input.value = "";
+    const messages = [...conversations, {role:"user", content: user_message}]
+    try {
+      await dispatch({
+        type: "ADD_CHAT",
+        payload: { role: "user", content: user_message },
+      });
 
-    dispatch({
-      type: "ADD_CHAT",
-      payload: { role: "user", content: user_message },
-    });
+      const completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: messages,
+      });
+
+      const model_message = completion.data.choices[0].message["content"];
+      console.log(model_message);
+      await dispatch({
+        type: "ADD_CHAT",
+        payload: { role: "assistant", content: model_message.trim() },
+      });
+      console.log(conversations);
+      input.value = "";
+      e.target.submit.disabled = false;
+      e.target.message.disabled = false;
+      setSending(false);
+    } catch (error) {
+      console.log(error);
+      if (error_count < 3) {
+        error_count += 1;
+        handleSubmit(e);
+      } else {
+        setError(true)
+        await dispatch({
+          type: "ADD_CHAT",
+          payload: {
+            role: "assistant",
+            content:
+              "There was a server overload. This is because we are currently on a free trial version of API. In future we are planning to have some custom models.",
+          },
+        });
+        e.target.submit.disabled = true;
+        e.target.message.disabled = true;
+        e.target.message.value = "ERROR";
+      }
+    }
   };
-
   return (
     <div className="container mx-auto px-6 py-4 h-screen flex flex-col mt-10">
       <div className="bg-white rounded-lg shadow-lg p-4 mt-4 w-full md:w-97 mx-auto flex-grow">
@@ -84,7 +86,7 @@ const ConversationPage = () => {
           ))}
           <div ref={messagesEndRef} />
         </div>
-        <form onSubmit={handleSubmitForm} className="flex mt-10 py-5">
+        <form onSubmit={handleSubmit} className="flex mt-10 py-5">
           <input
             type="text"
             name="message"
